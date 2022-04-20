ccea5e14aab71353b27f1ae133e7fe8d46345794
AldusD <aldusdanielfsm@gmail.com>
2022-04-20 12:25:48 -0300

    layout pagina inicial com placeholders, reset.css

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,6 @@
+// Variaveis globais e constantes
+const API = "https://mock-api.driven.com.br/api/v6/buzzquizz";
+
 // função para renderizar a página 1 da tela 3
 function renderCreateQuizPage() {
   const containerDiv = document.querySelector('.container');
@@ -18,3 +21,19 @@ function renderCreateQuizPage() {
 
   containerDiv.innerHTML += templateHTML;
 }
+
+// funções para renderizar seção todos os quizzes - tela inicial
+function insertQuizz (quizz, section) {
+  console.log("teste", quizz, section, "teste");
+  section.innerHTML += `<img scr="${quizz.image}">`;
+}
+
+function renderAllQuizzes() {
+  const quizzes = document.querySelector(".all-quizzes .quizzes");
+  console.log(quizzes);
+  const promise = axios.get(`${API}/quizzes`);
+
+  promise.then(response => response.data.map(insertQuizz.bind(null, quizzes)));
+}
+
+renderAllQuizzes()
